a56bb66ea3e993ffa73ceb6459a4fa7915ea5506
Gabriel Bury da Silva Santos <gabriel.santos@foster.srv.br>
2016-06-03 12:59:32 -0300

    First Commit

---
+++ Foster.SlidesToImages/Converter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+namespace Foster.SlidesToImages
+{
+    public class Converter
+    {
+        private String PathToDll { get; set; }
+        public Size NewSize { get; set; }
+
+        public Converter(string pathToLibrary = "")
+        {
+            if (!String.IsNullOrEmpty(pathToLibrary))
+            {
+                PathToDll = pathToLibrary;
+            }
+            else
+            {
+                PathToDll = System.Web.HttpContext.Current.Server.MapPath("~/FosterSlidesToImagesLib/");
+            }
+        }
+
+        /// <summary>
+        /// Convert specified PDF file to Images (1 image per pdf page)
+        /// </summary>
+        /// <param name="pdfPath"></param>
+        /// <returns></returns>
+        public IEnumerable<Image> ConvertPDFToImages(string pdfPath)
+        {
+            return ExtractImages(pdfPath);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        IEnumerable<Image> ExtractImages(string file)
+        {
+            var imagesList = new List<Image>();
+            Ghostscript.NET.Rasterizer.GhostscriptRasterizer rasterizer = null;
+            Ghostscript.NET.GhostscriptVersionInfo vesion = 
+                new Ghostscript.NET.GhostscriptVersionInfo(new Version(0, 0, 0), 
+                    PathToDll + @"\gsdll32.dll", 
+                    string.Empty, 
+                    Ghostscript.NET.GhostscriptLicense.GPL);
+
+            using (rasterizer = new Ghostscript.NET.Rasterizer.GhostscriptRasterizer())
+            {
+                rasterizer.Open(file, vesion, false);
+                for (int i = 1; i <= rasterizer.PageCount; i++)
+                {
+                    Image img = rasterizer.GetPage(300, 300, i);
+                    EncoderParameter qualityParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 60L);
+                    EncoderParameters encoderParams = new EncoderParameters(1);
+                    encoderParams.Param[0] = qualityParam;
+                    var imageStream = new MemoryStream();
+                    img.Save(imageStream, GetEncoderInfo("image/jpeg"), encoderParams);
+
+                    Image imageExported = new Bitmap(imageStream);
+                    if(NewSize != null)
+                    {
+                        imageExported = Util.ResizeImage(imageExported, NewSize);
+                    }
+                    imagesList.Add(imageExported);
+                }
+                rasterizer.Close();
+            }
+            return imagesList;
+        }
+
+        /// <summary> 
+        /// Returns the image codec with the given mime type 
+        /// </summary> 
+        private static ImageCodecInfo GetEncoderInfo(string mimeType)
+        {
+            // Get image codecs for all image formats 
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+
+            // Find the correct image codec 
+            for (int i = 0; i < codecs.Length; i++)
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+
+            return null;
+        }
+    }
+}
---
+++ Foster.SlidesToImages/Util.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foster.SlidesToImages
+{
+    public class Util
+    {
+        /// <summary>
+        /// Resizes the image to the specified size keeping the aspect ratio
+        /// </summary>
+        /// <param name="imgToResize"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static Image ResizeImage(Image imgToResize, Size size)
+        {
+            //Get the image current width
+            int sourceWidth = imgToResize.Width;
+            //Get the image current height
+            int sourceHeight = imgToResize.Height;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            if (size.Width > 0)
+            {
+                //Calulate  width with new desired size
+                nPercentW = ((float)size.Width / (float)sourceWidth);
+            }
+            if (size.Height > 0)
+            {
+                //Calculate height with new desired size
+                nPercentH = ((float)size.Height / (float)sourceHeight);
+            }
+
+            if (nPercentH < nPercentW && nPercentH > 0)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+
+            //New Width
+            int destWidth = (int)(sourceWidth * nPercent);
+            //New Height
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            // Draw image with new width and height
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+
+            return (Image)b;
+        }
+    }
+}
